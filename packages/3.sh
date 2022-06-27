9129e872c28cdfae96b2c644408178b6f4b1decd
Akhil Narang <me@akhilnarang.dev>
2022-06-27 17:32:47 +0530

    Add in a comment, add in a missing command from github's install docs
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- packages/3.sh
+++ packages/3.sh
@@ -8,9 +8,9 @@ sudo aptitude install libssl-dev -y
 echo "###############################################"
 echo "Done."
 
-echo -e "Install GitHub CLI"
-sudo apt-get install software-properties-common -y
+echo -e "Installing GitHub CLI"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update -y
 sudo apt install  gh -y
