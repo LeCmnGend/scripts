7781e2ce89b657cd5fa359c234eba8faf72e2716
LeCmnGend <namhoang1235@gmail.com>
2023-06-14 10:59:33 +0700

    setup: Install git-lfs  * Some custom rom sources started using git-lfs. Installing this Git extension will allow us to track those larger files from remote server
    Signed-off-by: Arrowsploit <arrowsploit@pm.me>

--- packages/1.sh
+++ packages/1.sh
@@ -1,6 +1,6 @@
 echo "0.1. Install requirement package"
 echo "###############################################"
-sudo aptitude install unzip zip cmake curl make git-core git gh wget tar  zstd -y
+sudo aptitude install unzip zip cmake curl make git-core git git-lfs gh wget tar  zstd -y
 sudo aptitude install build-essential flex ninja-build python-is-python3 ruby-full sqlite3 openjdk-11-jdk -y
 sudo aptitude install libxml-simple-perl libswitch-perl apt-utils -y
 echo "###############################################"
